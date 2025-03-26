8b482a8443cfe3520e9207ef5a53681d99938dee
Elfarie <nhthanhtung.iuh@gmail.com>
2025-03-26 19:44:19 +0700

    Add files via upload

---
+++ nhahang/src/main/java/connectDB/connectdata.java
@@ -0,0 +1,21 @@
+package connectDB;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class connectdata {
+		public static Connection getConnection() {
+			try {
+				String connectionUrl = "jdbc:sqlserver://stedy\\sqlexpress:1433;databaseName=RestaurantManagement;encrypt=true;trustServerCertificate=true;";
+				String userName="sa";
+				String password="1";
+				Connection connection = DriverManager.getConnection(connectionUrl,userName,password);
+				//System.out.println("thanh cong");
+				return connection;
+			
+			} catch (Exception e) {
+				e.printStackTrace();
+				return null;
+			}
+	}
+}
